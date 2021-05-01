dc4cfb41b3df1392c81955a9a8a35b9d480620ab
Ivan <ivan9274045452@gmail.com>
2021-05-01 22:41:59 +0300

    finish 00

--- DZ-2/00_distance.py
+++ DZ-2/00_distance.py
@@ -3,6 +3,8 @@
 
 # Есть словарь координат городов
 
+from pprint import pprint
+
 sites = {
     'Moscow': (550, 370),
     'London': (510, 510),
@@ -14,9 +16,30 @@ sites = {
 
 distances = {}
 
+msk = sites['Moscow']
+lnd = sites['London']
+prs = sites['Paris']
+
+msk_lnd = ((msk[0] - lnd[0]) ** 2 + (msk[1] - lnd[1]) ** 2) ** .5
+msk_prs = ((msk[0] - prs[0]) ** 2 + (msk[1] - prs[1]) ** 2) ** .5
+prs_lnd = ((prs[0] - lnd[0]) ** 2 + (prs[1] - lnd[1]) ** 2) ** .5
+
+distances['Moscow'] = {}
+distances['Moscow']['London'] = msk_lnd
+distances['Moscow']['Paris'] = msk_prs
+
+distances['London'] = {}
+distances['London']['Moscow'] = msk_lnd
+distances['London']['Paris'] = prs_lnd
+
+distances['Paris'] = {}
+distances['Paris']['Moscow'] = msk_prs
+distances['Paris']['London'] = prs_lnd
+
 # TODO здесь заполнение словаря
 
-print(distances)
+
+pprint(distances)
 
 
 
